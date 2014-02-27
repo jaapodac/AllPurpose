ee18c109c9e0a6fac9417d321c27ce3ed3ef1ee1
J. Apodaca <jaapodac@gmail.com>
2014-02-26 21:53:44 -0700

    Added Documentation and three functions

--- js/AllPurpose.js
+++ js/AllPurpose.js
@@ -1,6 +1,26 @@
 // JavaScript Document
+
+/* -----------+ Comment Block +-------------------
+File:		AllPurpose.js
+Author:     J. Apodaca
+Date:       Feb 26, 2014
+Purpose:    Generic JavaScript file to be copied as a template in other projects 
+Dependedncies: None
+    Input:     None
+Output:        None
+
+Example of Use: <script type="text/javascript" src="js/AllPurpose.js"> </script>
+
+Special Thanks to: J. Apodaca
+
+*/
+
+//Note: Unlike PHP scripts, we do NOT use <script> pairs with Javascript
  
     function hello(message){
-        return "hows it going I want to give myself a message which is this: " + message;
+        return "This is a External (file) JavaScript Function that bears a message: " + message;
     }
-    document.getElementById("testing").innerHTML = hello("You're silly");
+    
+    document.getElementById("container").innerHTML = hello("You're silly");
+    	
+
